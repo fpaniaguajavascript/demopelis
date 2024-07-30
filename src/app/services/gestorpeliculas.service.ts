cea417bae22705f5f6c80fc6d2b8c7ef6143482e
fpaniaguajavascript <fernando.paniagua.javascript@gmail.com>
2024-07-30 18:01:41 +0200

    Update gestorpeliculas.service.ts

--- src/app/services/gestorpeliculas.service.ts
+++ src/app/services/gestorpeliculas.service.ts
@@ -5,9 +5,23 @@ import { Ipelicula } from '../interfaces/ipelicula';
   providedIn: 'root'
 })
 export class GestorpeliculasService {
+  private static LS_KEY = "peliculas";
   public peliculas: Ipelicula[] = [];
-  constructor() { }
+  constructor() {
+    this.load();
+  }
   public add(nuevaPelicula: Ipelicula): void {
     this.peliculas.push(nuevaPelicula);
+    this.save();
+  }
+  private load() {
+    const data = localStorage.getItem(GestorpeliculasService.LS_KEY);
+    if (data != null) {
+      this.peliculas = JSON.parse(data);
+    }
+  }
+  private save() {
+    const data = JSON.stringify(this.peliculas);
+    localStorage.setItem(GestorpeliculasService.LS_KEY, data);
   }
 }
